8146371d548b456eb94ec22eff383afdd049c0f0
DakshSharma04022008 <ds04022008@gmail.com>
2026-04-09 19:21:39 +0530

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,10 @@
+// Welcome message when site loads
+window.onload = function () {
+    console.log("Welcome to Bake Box Website");
+};
+
+// Contact page button message
+function showMessage() {
+    document.getElementById("msg").innerHTML =
+        "Thank you for contacting Bake Box. We will get back to you soon!";
+}
